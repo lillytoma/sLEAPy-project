b047f57611368ae1b570825796b06aab8a1d0fdb
Lilyan Toma <123890501+lillytoma@users.noreply.github.com>
2026-09-12 17:39:34 -0500

    Added comments to the last 2 created files from my previous commit. The two files being: LoginRequstDTO and AuthController

--- starter/backend/src/main/java/com/sleapy/project/models/LoginRequestDTO.java
+++ starter/backend/src/main/java/com/sleapy/project/models/LoginRequestDTO.java
@@ -7,8 +7,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+//This class represents a data transfer object (DTO) for login requests. It contains two fields: email and password, which are used to capture the user's login credentials when they attempt to authenticate with the system.
 public class LoginRequestDTO {
-
+    //The email field stores the user's email address, which is used as the unique identifier for authentication purposes.
     private String email;
     private String password;
 
