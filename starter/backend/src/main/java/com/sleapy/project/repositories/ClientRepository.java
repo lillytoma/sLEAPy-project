599f36d4b0176d0728e2369679ca609a5690adc8
Lilyan Toma <123890501+lillytoma@users.noreply.github.com>
2026-09-12 17:13:09 -0500

    added findByEmail function to ClientRepository to issue a SQL query against the clients table ver the connection which will return if the client has or does not have an email

--- starter/backend/src/main/java/com/sleapy/project/repositories/ClientRepository.java
+++ starter/backend/src/main/java/com/sleapy/project/repositories/ClientRepository.java
@@ -1,10 +1,10 @@
 package com.sleapy.project.repositories;
-
+import java.util.Optional;
 import com.sleapy.project.models.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
-    
+    Optional<ClientEntity> findByEmail(String email);
 }
