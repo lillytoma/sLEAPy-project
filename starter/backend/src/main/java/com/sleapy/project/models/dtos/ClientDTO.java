e9c7288cb1b6c5c05ea17b122a734461c6c60c1a
RobbieT17 <robbiewthurston@gmail.com>
2026-09-18 19:21:17 +0000

    removed final fields

--- starter/backend/src/main/java/com/sleapy/project/models/dtos/ClientDTO.java
+++ starter/backend/src/main/java/com/sleapy/project/models/dtos/ClientDTO.java
@@ -2,19 +2,21 @@ package com.sleapy.project.models.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor 
 @AllArgsConstructor 
 public class ClientDTO {
 
-    private final Long id;
-    private final String email;
-    private final String username;
-    private final double cashBalance;
-    private final String phoneNumber;
-    private final String ssnLast4;
-    private final String fullAddress;
-    private final String birthDate; // YYYY-MM-DD
+    private Long id;
+    private String email;
+    private String username;
+    private double cashBalance;
+    private String phoneNumber;
+    private String ssnLast4;
+    private String fullAddress;
+    private String birthDate; // YYYY-MM-DD
 
     // private final Portfolio portfolio;
    
