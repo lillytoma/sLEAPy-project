1d4375ffdf043f553a57e81ea6f0644d483de5c8
Ianj751 <ianj60184@gmail.com>
2026-09-23 15:07:00 +0000

    fix: unnecessary import removed

--- starter/backend/src/main/java/com/sleapy/project/controllers/AuthController.java
+++ starter/backend/src/main/java/com/sleapy/project/controllers/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
